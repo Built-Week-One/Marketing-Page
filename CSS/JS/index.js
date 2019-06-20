b5e1b5d7ae475c47822a3d3e44814240dd54d314
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-06-19 19:36:58 -0700

    Commit

---
+++ CSS/JS/index.js
@@ -0,0 +1,72 @@
+// Index.js - By Carl Stanley 
+// Start with Site Content. 
+
+const siteContent = {
+
+    "a" : {
+
+        "0" : "index.html", // Home Page
+        "1" : "about.html", // About page
+        "2" : "contact.html" // Contact Page
+
+    },
+    
+    "button" : {
+        // Index Buttons
+        "0" : "Donors Click Here to Begin",
+        "1" : "School Admins Click Here",
+        "2" : "Schools Click here to List Your Needs",
+        "3" : "Contact US",
+        "4" : "ABOUT"
+       
+        
+    }, 
+    
+    "footer" : {
+        // Footer all pages
+        "0" : "Copyright © 2019"
+    },
+
+    "H" : {
+        // Headers for Index Page
+        "0" : "LUNCHER",
+        "1" : "We help donors provide hot meals to school children in need."
+    },  
+    
+    
+    // Images 
+    "img" : {
+        
+        "0" : "img/nav-hamburger.png",
+        "1" : "img/nav-hamburger-close.png",
+        "2" : "img/home/home-img-1.png",
+        "3" : "img/home/home-img-2.png",
+        "4" : "img/home/home-jumbotron.png",
+        "5" : "img/home/home-mobile-img-1.png",
+        "6" : "img/home/home-mobile-img-2.png",
+        "7" : "img/home/home-mobile-jumbotron.png",
+        "8" : "img/home/home-mobile-outskirts-img.png"
+        
+
+    },
+    
+    "menu" : { // Menu all Pages 
+
+        "0" : "HOME",
+        "1" : "ADMIN",
+        "2" : "DONOR",
+        "3" : "CONTACT",
+        "4" : "ABOUT"
+
+    },    
+
+    "p" : {
+        // P for Index Page
+        
+        "0" : "Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer sed. Duo etiam laboramus dissentiet in, nec no errem",
+        "1" : "Et sed autem causae appareat, tempor abhorreant te mei, facer facilisis sit ea. Eu timeam vidisse consectetuer sed. Duo etiam laboramus dissentiet in, nec no errem"
+
+        
+    }    
+
+};
