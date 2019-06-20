56a4dfc75b70544386e0a82e30ceb1faf823b1d2
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-06-19 19:48:26 -0700

    Adding Images

--- JS/Index.js
+++ JS/Index.js
@@ -37,15 +37,15 @@ const siteContent = {
     // Images 
     "img" : {
         
-        "0" : "img/nav-hamburger.png",
-        "1" : "img/nav-hamburger-close.png",
-        "2" : "img/home/home-img-1.png",
-        "3" : "img/home/home-img-2.png",
-        "4" : "img/home/home-jumbotron.png",
-        "5" : "img/home/home-mobile-img-1.png",
-        "6" : "img/home/home-mobile-img-2.png",
-        "7" : "img/home/home-mobile-jumbotron.png",
-        "8" : "img/home/home-mobile-outskirts-img.png"
+        "0" : "img/",
+        "1" : "img/",
+        "2" : "img/",
+        "3" : "img/",
+        "4" : "img/",
+        "5" : "img/",
+        "6" : "img/",
+        "7" : "img/",
+        "8" : "img/"
         
 
     },
