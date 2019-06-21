bf0504950c5090d388d6fea3290553b78b50a804
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-06-20 18:38:41 -0700

    Commit

--- JS/Index.js
+++ JS/Index.js
@@ -2,46 +2,6 @@
 // Start with Site Content. 
 
 
-// Get Button text from siteContect obj
-
-//let paragraph = siteContent.paragraphs;
-
-//let newP1 = document.createElement("p");
-
-//newP1.setAttribute('innerHTML',paragraph[0]);
-
-//let btmTxt = document.getElementsByClassName('btm-text');
-
-//btmTxt.paragraph
-
-const navigation = document.querySelector('Nav');
-
-let Nav = document.getElementsByTagName('Nav');
-
-let newLink1 = document.createElement("a");
-
-let newLink2 = document.createElement("a");
-
-let newLink3 = document.createElement("a");
-
-let newLink4 = document.createElement("a");
-
-let newLink5 = document.createElement("a");
-
-newLink1.innerHTML = siteContent.menu[0];
-newLink1.setAttribute('href',`${siteContent.a[0]}`);
-newLink1.style.color = '#212529';
-newLink1.style.fontSize = '1.6rem';
-
-newLink2.innerHTML = siteContent.menu[1];
-newLink2.setAttribute('href',`${siteContent.a[1]}`);
-newLink2.style.color = '#212529';
-newLink2.style.fontSize = '1.6rem';
- 
-
-
- 
-
 
  
 
